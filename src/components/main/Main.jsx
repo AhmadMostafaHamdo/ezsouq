d164153de479f4e6e5e12affe3174932eb5485e0
Ahmad <ahmadhamdo5123@gmail.com>
2025-06-23 12:35:03 -0400

    responsive in home for mobile

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,28 +1,27 @@
 import { Link } from "react-router";
 import Lottie from "lottie-react";
 import animationData from "../../assets/lottifiles/web home animation.json";
-
 const Main = () => {
   return (
-    <div className="h-[636px] w-[100vw] text-white font-sans  flex justify-between  bg-primary">
+    <div className="md:h-[636px] w-[100vw] p-4 md:p-0 text-white font-sans flex-col-reverse md:flex-row flex   bg-primary">
       <div className="w-[50vw" style={{ paddingRight: "50px" }}>
-        <h1 className="text-[24px] md:text-[36px] text-nowrap font-[800] mt-[161px]">
+        <h1 className="text-[24px]  md:text-[36px] text-nowrap font-[800] md:mt-[161px]">
           بيع، اشتري، وابدأ تجارتك من مكانك
         </h1>
         <p className="w-[464px] font-bold mt-[35px] text-[1rem]">
-          كل ما تحتاجه من عقارات، سيارات، إلكترونيات وأكثر  <br/>في منصة سورية
-          موثوقة.
+          كل ما تحتاجه من عقارات، سيارات، إلكترونيات وأكثر <br />
+          في منصة سورية موثوقة.
         </p>
         <Link
           to=""
-          className="rounded-xl mt-[55px] bg-white w-[262px] h-[70px] font-bold text-primary block flex-center text-[16px]"
+          className="rounded-xl mt-[55px] bg-white w-[262px] h-[70px] font-bold text-primary block flex-center text-[1rem]"
         >
           نشر إعلان
         </Link>
       </div>
-      <div className="w-full relative " >
-        <div className="absolute w-[72%]  transform   -translate-x-[230px] -translate-y-[80px]">
-          <Lottie animationData={animationData} loop={false}  />
+      <div className="w-full flex justify-center md:justify-end items-center">
+        <div className="w-[70%]  transform   md:translate-x-[0px] md:-translate-y-[11vh]">
+          <Lottie animationData={animationData} loop={false} />
         </div>
       </div>
     </div>
